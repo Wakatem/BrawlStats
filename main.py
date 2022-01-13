ec2f2ec3c9429cd5c7dd1dfaa359a72458e50334
Wakatem <moody-&@live.com>
2022-01-13 18:07:11 +0400

    Create a window

---
+++ main.py
@@ -0,0 +1,12 @@
+#imports
+import tkinter as tk
+from tkinter import ttk
+import requests as req
+
+
+#window setup
+root = tk.Tk()
+root.title("BrawlStats")
+root.geometry("1600x900+500+400")
+
+root.mainloop()
