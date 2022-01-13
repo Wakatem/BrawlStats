379024b2f80e2a9d228453153e2b8df5d581e7aa
Wakatem <moody-&@live.com>
2022-01-13 18:09:51 +0400

    Add widgets

--- main.py
+++ main.py
@@ -13,4 +13,16 @@ root = tk.Tk()
 root.title("BrawlStats")
 root.geometry("1600x900+500+400")
 
+detailsFrame = ttk.Frame(root, height=700)
+detailsFrame.pack()
+
+message = ttk.Label(root, text="Enter Brawlhalla ID:")
+message.pack()
+
+textbox = ttk.Entry()
+textbox.pack(pady=(15, 25))
+
+button = ttk.Button(root, text="Search")
+button.pack()
+
 root.mainloop()
